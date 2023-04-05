3a043d38f21f7ac6b97533e328c4933fadc95916
Purityndirangu <ndiranguwanjikupurity@gmail.com>
2023-04-05 05:52:32 +0300

    The use of Map()

--- syntax.js
+++ syntax.js
@@ -46,3 +46,39 @@ if (a < b) {
 }else {
     console.log(`${a} and ${b} are equal.`);
 }
+
+//The Map object holds key-value pairs 
+//and remembers the original insertion order of the keys.
+//Herre, it creates a new array with the results of 
+//calling a provided function on every element in the calling array
+const nums = [1,2,3,4];
+const doubleNums = nums.map
+((num) => num * 2 );
+console.log({doubleNums});
+
+//essentially
+//new Map ()     //Creates a new map
+//set ();        //Sets the value for akey in a map
+//get();         //gets the value for a key in a map
+//delete();     Removes a Map element specified by the key
+//has();         //returns true if a key exists in a map
+//forEach()	   //Calls a function for each key/value pair in a Map
+//entries()	  //Returns an iterator with the [key, value] pairs in a Map
+//size	      //Returns the number of elements in a Map
+
+const container = new Map();
+
+container.set("fruits",4);
+container.set("vegies",2);         // can also be used to change existing Map values
+console.log(container.get("fruits"));
+console.log(container.size);
+console.log(container.delete("vegies"));
+console.log( container.has("vegies"));
+const addTo = container.entries();
+console.log(addTo.next().value);        //It returns a new iterable iterator object.
+container.set("furniture",45);
+container.set("paint",32)
+console.log(addTo.next().value); 
+console.log(addTo.next().value); 
+console.log(container.get("furniture"));
+console.log(container.get("paint"));
